e5cdf3669a844a1200680afc4a7c11685981df2c
zhangxx <zhangxx.123>
2018-04-18 22:26:47 +0800

    新增textview

--- aidlDemo/AidlServiceDemo/src/com/example/aidlservicedemo/MainActivity.java
+++ aidlDemo/AidlServiceDemo/src/com/example/aidlservicedemo/MainActivity.java
@@ -5,11 +5,13 @@ import android.app.Activity;
 
 
 public class MainActivity extends Activity  {
-
+private  TextView   mTextview;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		mTextview=new Textview(this);
+		mTextview.setText("12334");
 	}
 
 	
